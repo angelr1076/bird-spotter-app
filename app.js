2222d85cc939f327a04ff7bef514fc0b9b54ecb5
Angel Rodriguez <angelr1076@gmail.com>
2019-06-29 11:38:48 -0400

    Added bird favicon

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// require dotenv to loads environment variables
+// Require dotenv to loads environment variables
 require("dotenv").config();
 
 const express = require("express"),
@@ -11,10 +11,9 @@ const express = require("express"),
   methodOverride = require("method-override"),
   flash = require("connect-flash"),
 
-  //requiring routes
+  //require routes
   commentRoutes = require("./routes/comments"),
   birdRoutes = require("./routes/birds"),
-  //Testing user routes
   userRoutes = require("./routes/users"),
   indexRoutes = require("./routes/index");
 
@@ -37,10 +36,11 @@ app.use(bodyparser.urlencoded({
 
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-app.use(flash()); //needs to come before passport configuration
+// Needs to come before passport configuration
+app.use(flash());
 app.locals.moment = require("moment");
 
-//configure passport
+// Configure passport
 app.use(
   require("express-session")({
     secret: process.env.SECRET,
@@ -55,7 +55,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//create middleware to pass in to every route, this way we dont have to pass a user object to every route
+// Create middleware to pass in to every route
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.error = req.flash("error");
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
   next();
 });
 
-//tell our app to use these three routes. This needs to come after previous app.use();
+// Needs to come after previous app.use();
 app.use("/", indexRoutes);
 app.use("/users", userRoutes);
 app.use("/birds", birdRoutes);
